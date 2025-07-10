7c9e54bd0442af7131c8990f95a2e6efac32e9ed
ronaldkwan93 <ronaldkwan93@gmail.com>
2025-07-11 01:53:59 +1000

    adds home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,37 @@
+import styles from "./Home.module.scss";
+
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__heading}>
+        <h1>Postcode API</h1>
+        <p>Your comprehensive postcode and suburb lookup service</p>
+      </div>
+      <div>
+        <h2>I want to...</h2>
+        <div>
+          <div className={styles.container__action}>
+            <h3>Choose an action</h3>
+            <p>▼</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.container__info}>
+        <div className={styles.container__info__box}>
+          <p>⚡</p>
+          <div>Lightning Fast</div>
+        </div>
+        <div className={styles.container__info__box}>
+          <p>🎯</p>
+          <div>Accurate Data</div>
+        </div>
+        <div className={styles.container__info__box}>
+          <p>🔧</p>
+          <div>Easy to Use</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
